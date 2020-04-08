465173a8e1ea2b7d023cbb1bb81f1f574660085e
Exploit-Injector <55884881+Exploit-Injector@users.noreply.github.com>
2020-04-08 14:37:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?
+// Ambil User Agent dari config sc lama.
+// Buat yang belum pernah daftar. Capture dulu ambil User Agentnya agar tidak system tidak mendetek penyalahgunaan
+
+$UserAgent = "xxx";
+$Email = "xxxx";
+$Password = "xxxx";
